817b83f0aed8d7cf13580f79779dfd90b91c53ef
David Andrade <66143693+Davidandrade01@users.noreply.github.com>
2024-08-06 03:15:16 +0100

    Merge branch Larissa
    Merge da branch Larissa com a Branch David

--- script/index/main.js
+++ script/index/main.js
@@ -21,25 +21,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
       switch (value) {
         case 'blog':
-          url = './blog.html'; 
+          url = '../../blog.html'; 
           break;
         case 'Consultores':
-          url = '/consultores'; 
+          url = '../../consultores.html'; 
           break;
         case 'Contatos':
-          url = '/contatos'; 
+          url = '../../contacto.html'; 
           break;
         case 'Clientes':
-          url = '/clientes'; 
+          url = '../../index.html'; 
           break;
         case 'Livro de reclamação':
-          url = '/livro-de-reclamacao'; 
+          url = 'https://bespoke.deco.proteste.pt/2023/marketplace?key=UgTbQ7vTi7Wa5cruw7WSbuDBdVo&gad_source=1&gclid=Cj0KCQjw8MG1BhCoARIsAHxSiQnRUCA1qF5TFpdliV7RsIvW6QjskGGhHZazioypvV-Lna9miLSFED8aAo89EALw_wcB&site_name=Marketplace&utm_campaign=PTREGSE9P&utm_medium=cpc&utm_source=google' ; 
           break;
         case 'Serviços Financeiros':
-          url = '/servicos-financeiros'; 
+          url = '../../servicos.html'; 
           break;
         case 'Sobre nós':
-          url = '/sobre-nos'; 
+          url = '../../servicos.html'; 
           break;
         default:
           url = null;
